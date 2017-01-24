8e52038d572297cebfb091cc89f880cbbed9d5f4
aliciabigley <aliciabigley@gmail.com>
2017-01-24 08:05:48 -0600

    Search my name or trait function set up

--- gui.js
+++ gui.js
@@ -1,10 +1,24 @@
-function initSearch(people){
+function initSearch(people, data){
+var searchType = prompt("What would you like to search by Name [1] or Traits [2]?\n\nName includes First and Last, Kin, Descendants, and Immediate Family Members");
+	switch(searchType){
+		case "1":
+			initSearchByName(data);
+			break;
+		case "2":
+			initSearchByTraits(data);
+			break;
+		default:
+			alert("Oops! That's not an option. Try [1] or [2] next time.");
+			initSearch(people, data);
+			break;
+	}
+}
+function initSearchByName(data){
+	var name = promtp("Please type in the name of the person you would like to search.")
+	alert = data.find(name);
 	;
 }
-function initSearchByName(){
-	;
-}
-function initSearchByTraits(){
+function initSearchByTraits(data){
 	;
 }
 function displayResults(){
