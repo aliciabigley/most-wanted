289e77ae30a3a494976371d50d3eedb6aeb0c0c9
aliciabigley <aliciabigley@gmail.com>
2017-01-24 20:39:39 -0600

    working on prompts for traits.

--- gui.js
+++ gui.js
@@ -30,36 +30,56 @@ function initSearchByName(people, firstName, lastName){
 		}
 	});
 	alertNameSearch(people, nameSearchValue)
-	//when I get here, it shoots back to initSearch
 }
 
 function alertNameSearch(people, nameSearchValue) {
-	alert(nameSearchValue);
+	try{
+	alert("Here are your search results:\n" + nameSearchValue[0].firstName + " " + nameSearchValue[0].lastName);
+
+	}
+	catch(err){
+			alert("We couldn't find anyone with that name on the Most Wanted List. ");
+			initSearch(people);
+	}
 	}
 
-// function initSearchByTraits(people){
-// 	var searchType; = prompt("What would you like to search by?\n\n Age [1], Height [2], Weight [3], Eye Color [4], Occupation [5]");
-// 	switch(searchType){
-// 		case "1":
-// 			getAge();
-// 			break;
-// 		case "2":
-// 			getHeight();
-// 			break;
-// 		case "3":
-// 			getWeight();
-// 			break;
-// 		case "4":
-// 			getEye();
-// 			break;
-// 		case "5":
-// 			getOccupation();
-// 			break;
-// 		default:
-// 			alert("Oops! That's not an option.");
-// 			initSearchByTraits(people);
-// 			break;
-// 	;
+function initSearchByTraits(people){
+	var searchTraits = prompt("What would you like to search by?\n\n Age [1], Height [2], Weight [3], Eye Color [4], Occupation [5]");
+	switch(searchType){
+		case "1":
+			agePrompt(people);
+			break;
+		case "2":
+			heightPrompt(people);
+			break;
+		case "3":
+			weightPrompt(people);
+			break;
+		case "4":
+			eyePrompt(people);
+			break;
+		case "5":
+			occupationPrompt(people);
+			break;
+		default:
+			alert("Oops! That's not an option.");
+			initSearchByTraits(people);
+			break;
+		}
+	}
+
+function agePrompt(people){
+var age = prompt("Please enter the age of the person you would like to search for.");
+getAge(people, age)
+
+}	
+function getAge(people, age){
+//pull people.dob
+// split by /
+//dob year - year = age
+	;
+}
+
 // }
 // function displayResults(){
 // 	;
@@ -67,18 +87,57 @@ function alertNameSearch(people, nameSearchValue) {
 // function isNumeric(){
 // 	;
 // }
-// function getAge(){
-// 	;
-// }
+function heightPrompt(people){
+var height = prompt("Please enter the height of the person you would like to search for.");
+getAge(people, height)
+
+}
 // function getHeight(){
 // 	;
 // }
+function weightPrompt(people) {
+var weight = prompt("Please enter the weight of the person you would like to search for.");
+getAge(people, weight)
+}
+
 // function getWeight(){
 // 	;
 // }
-// function getEye(){
-// 	;
-// }
-// function getOccupation(){
-// 	;
-// }
+function eyePrompt(people) {
+var eyeColorSearch = prompt("Please enter the color of the person's eyes you would like to search for.");
+getAge(people, eyeColor)
+}
+function getEye(people, eyeColorSearch){
+var eyeColorResult = people.filter(function(color){
+	if(color.eyeColorSearch.toLowerCase() == eyeColor.toLowerCase()){
+		return true;
+	}
+	else{
+		return false;
+	}
+});
+alertEyeSearch(people, eyeColorResult)
+}
+
+function alertEyeSearch(people, eyeColorResult){
+	try{
+		alert("Here are your search results:\n" eyeColorResult[0].firstName + " " + eyeColorResult[0].lastName)
+	}
+	catch(err){
+		alert("We couldn't find anyone with that those colored eyes on the Most Wanted List.")
+	}
+}
+function occupationPrompt(people){
+var occupationSearch = prompt("Please enter the occupation of the person you would like to search for.");
+getOccupation(people, occupationSearch)
+}
+function getOccupation(people, occupationSearch){
+	var occupationResult = people.filter(function(occupationElement){
+	if(occupationElement.occupationResult.toLowerCase() == occupation.toLowerCase()){
+		return true;
+	}
+	else{
+		return false;
+	}
+});
+}
